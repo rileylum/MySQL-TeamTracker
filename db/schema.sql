5b884a1ccbc2c70c43db3fddeb2dde5cf45a07e5
Riley Lum <rileylum@adam.com.au>
2021-08-24 17:33:36 +0930

    auto increment primary keys

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE team_db;
 USE team_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department INT NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
