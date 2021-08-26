e0910d22344493637a50315a0ddd1b26a384eaab
Riley Lum <rileylum@adam.com.au>
2021-08-26 18:19:05 +0930

    add sql query functions

--- utils/queries.js
+++ utils/queries.js
@@ -9,7 +9,7 @@ async function openDBConnection() {
         database: process.env.DB_DATABASE
     });
     return db;
-}
+};
 
 async function getAll(table) {
     const db = await openDBConnection();
@@ -17,6 +17,47 @@ async function getAll(table) {
     const [result] = await db.query(queryString);
     db.end();
     return result;
-}
+};
+
+async function addEmployee(input) {
+    const db = await openDBConnection();
+    queryString = `INSERT INTO employee(first_name, last_name, role_id, manager_id)
+                VALUES (?,?,?,?)`;
+    const [result] = await db.query(queryString, input);
+    db.end();
+    return result;
+};
+
+async function addRole(input) {
+    const db = await openDBConnection();
+    queryString = `INSERT INTO role(title, salary, department_id)
+                VALUES (?,?,?)`;
+    const [result] = await db.query(queryString, input);
+    db.end();
+    return result;
+};
+
+async function addDepartment(input) {
+    const db = await openDBConnection();
+    queryString = `INSERT INTO department(name)
+                VALUES (?)`;
+    const [result] = await db.query(queryString, input);
+    db.end();
+    return result;
+};
+
+async function updateEmployeeRole(input) {
+    const db = await openDBConnection();
+    queryString = `UPDATE employee
+                SET role_id = ?
+                WHERE id = ?`;
+    const [result] = await db.query(queryString, input);
+    db.end();
+    return result;
+};
 
 module.exports.getAll = getAll;
+module.exports.addEmployee = addEmployee;
+module.exports.addRole = addRole;
+module.exports.addDepartment = addDepartment;
+module.exports.updateEmployeeRole = updateEmployeeRole;
