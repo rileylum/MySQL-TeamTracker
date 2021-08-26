b5841102a085eb41001b01042a7822ce2f1a691f
Riley Lum <rileylum@adam.com.au>
2021-08-26 17:33:20 +0930

    move database queries to seperate file

---
+++ utils/queries.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+async function openDBConnection() {
+    const db = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE
+    });
+    return db;
+}
+
+async function getAll(table) {
+    const db = await openDBConnection();
+    queryString = `SELECT * FROM ${table}`
+    const [result] = await db.query(queryString);
+    db.end();
+    return result;
+}
+
+module.exports.getAll = getAll;
